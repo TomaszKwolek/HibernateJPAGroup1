06f4461b0781bc32e60dadc0d8cb1525826e784b
TomaszKwolek <tomasz.kwolek@capgemini.com>
2016-05-22 21:06:09 +0200

    refactor

--- src/test/java/pl/employees/service/EmployeeServiceTest.java
+++ src/test/java/pl/employees/service/EmployeeServiceTest.java
@@ -2,7 +2,6 @@ package pl.employees.service;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.text.DateFormat;
 import java.text.ParseException;
